18111ede9e8ac79cc64ea972ea31de9e78fa2bbb
ssathya <ssathya@ncsu.edu>
2015-02-08 14:59:18 -0500

    Changes to shell script

--- myshell.sh
+++ myshell.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 git clone https://github.com/SkymindIO/nd4j
 cd nd4j
-mvn clean install -DskipTests -Dmaven.javadoc.skip=true
-echo "Successfully executed the build script"
+for i in 'seq 1 5';
+do
+	echo $i
+done
+echo "Successfully executed the shell script"
